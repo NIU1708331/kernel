e4354b54e8403dea98e5f42e65a20c91c7002b9b
Dsolm <dsola2405@gmail.com>
2024-07-11 18:33:38 +0200

    Added interrupt handling

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,29 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
+#![test_runner(kernel::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
-mod serial;
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
 use core::panic::PanicInfo;
+use kernel::println;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello world!");
+    kernel::init();
+
+    // fn stack_overflow() {
+    //     stack_overflow();
+    // }
+
+    // stack_overflow();
+
+    #[cfg(test)]
+    test_main();
+
+    println!("Kernel didn't crash");
+    loop {}
+}
 
 #[cfg(not(test))]
 #[panic_handler]
@@ -35,46 +35,5 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    #[cfg(test)]
-    test_main();
-
-    println!("Hello{}", "!");
-    loop {}
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1,1);
+    kernel::test_panic_handler(info);
 }
